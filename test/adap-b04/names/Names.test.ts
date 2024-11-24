00b4df39032a384270c893c309e5b2fd021f3fe1
jiorgos2811 <gkesaris00@gmail.com>
2024-11-24 23:21:58 +0100

    fixed paths

--- test/adap-b04/names/Names.test.ts
+++ test/adap-b04/names/Names.test.ts
@@ -1,14 +1,8 @@
 import { describe, it, expect, beforeEach } from 'vitest';
-<<<<<<< HEAD
-import { StringName } from '../../../src/adap-b04/names/StringName';
-import { StringArrayName } from '../../../src/adap-b04/names/StringArrayName';
-import { IllegalArgumentException } from '../../../src/adap-b04/common/IllegalArgumentException';
-=======
 import { StringName } from './StringName';
 import { StringArrayName } from './StringArrayName';
 import { IllegalArgumentException } from '../common/IllegalArgumentException';
 import { InvalidStateException } from '../common/InvalidStateException';
->>>>>>> af6af2b (Names implementation b04)
 
 describe('Name Implementation Tests', () => {
     describe('StringName Contract Tests', () => {
@@ -24,13 +18,8 @@ describe('Name Implementation Tests', () => {
 
         it('should maintain invariant after operations', () => {
             name.append("com");
-<<<<<<< HEAD
-            expect(name.getNoComponents()).toBe(4);
-            expect(name.getComponent(3)).toBe("com");
-=======
             expect(name.getNoComponents()).toBe(3);
             expect(name.getComponent(2)).toBe("com");
->>>>>>> af6af2b (Names implementation b04)
         });
 
         it('should throw on invalid operations', () => {
